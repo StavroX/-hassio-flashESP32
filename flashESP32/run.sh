65b5af5d51522215b90abd9623fae073e44bf5a6
Jakob Theodosiou <jakobtheodosiou@gmail.com>
2022-02-23 01:32:57 +0100

    Initial commit

---
+++ flashESP32/run.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+echo "Started"
+cd /esptool
+
+echo "Downloading ESP32 Bootloader"
+if ! wget https://github.com/Aircoookie/WLED/releases/download/v0.13.0-b6/esp32_bootloader_v4.bin; then echo "bootloader not found" && exit 1; fi
+
+echo "Downloading WLED"
+if ! wget https://github.com/Aircoookie/WLED/releases/download/v0.13.0-b6/WLED_0.13.0-b6_ESP32.bin; then echo "bootloader not found" && exit 1; fi
+
+echo "Do button magic to make PI recognize ESP32. You Have 5 seconds.."
+sleep 5
+./esptool.py get_security_info
+
+
+# if ! ./cc_chipid | grep "ID = b524"; then echo "ChipID not found." && exit 1; fi
+
+
+
+# echo "Downloading firmware"
+# if ! wget https://github.com/Koenkk/Z-Stack-firmware/blob/master/coordinator/Z-Stack_Home_1.2/bin/default/CC2531_DEFAULT_20211115.zip?raw=true; then echo "firmware not found" && exit 1; fi
+
+# echo "unziping"
+# mv CC2531_DEFAULT_20211115.zip?raw=true CC2531_DEFAULT_20211115.zip
+# if ! 7z x CC2531_DEFAULT_20211115.zip; then echo "unzip failed" && exit 1; fi
+
+# echo "backup firmware"
+# ./cc_read save.hex
+# if ! cp save.hex /backup/save_$(date +%s).hex; then echo "could not backup firmware" && exit 1; fi
+
+# echo "erase"
+# ./cc_erase
+
+# echo "flash firmware"
+# ./cc_write CC2531ZNP-Prod.hex
+
+# echo "Finished"
